58bb903c61d3d9965238a88aff780446f3885411
Wppopqpu <wppopqpu@outlook.com>
2023-04-14 20:42:59 +0800

    Add Basic.lua(uncompleted), Entry.lua(empty). I'm trying to write some configuration, but I still can't understand much.

---
+++ Basic.lua
@@ -0,0 +1,43 @@
+
+-- reserved lines for j's and k'
+vim.o.scrolloff = 8
+vim.o.sidescrolloff = 8
+
+-- line numbers
+vim.wo.numver = true
+vim.wo.relativenumber = true
+
+-- cursor line highlight
+vim.wo.cursorline = true
+
+-- sign column
+vim.wo.signcolumn = "yes"
+
+
+local basicLength = 4
+
+-- tabs
+vim.o.tabstop = basicLength
+vim.bo.tabstop = basicLength
+vim.o.softtabstop = basicLength
+vim.o.shifttound = true
+vim.o.expandtab = false
+vim.bo.expandtab = false
+
+-- the length to move when >> or <<
+vim.o.shiftwidth = basicLength
+vim.bo.shiftwidth = basicLength
+
+-- auto indent
+vim.o.autoindent = true
+vim.bo.autonindent = true
+vim.o.smartindent = true
+
+-- whether to ignore case
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- search options
+vim.o.hlsearch = true
+vim.o.insearch = true
+
