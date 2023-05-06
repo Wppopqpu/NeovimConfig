968dc764de6a785e7c3438419617adcd3f5b3fda
Wppopqpu <wppopqpu@outlook.com>
2023-05-06 17:48:42 +0800

    add gitsigns and some basic lsp plugins

--- Core/Plugins.lua
+++ Core/Plugins.lua
@@ -1,17 +1,28 @@
 -- Plugins.lua
 return require( 'packer' ).startup(function( use )
 	use 'wbthomason/packer.nvim'
-
 	use 'folke/tokyonight.nvim'
+	use 'lukas-reineke/indent-blankline.nvim'
+	use 'lewis6991/gitsigns.nvim'
+	use {'neovim/nvim-lspconfig', 'williamboman/nvim-lsp-installer'}
+
+
+
 	vim.cmd( 'colorscheme tokyonight' )
-
-
-
-	use "lukas-reineke/indent-blankline.nvim"
-		vim.opt.list = true
+	vim.opt.list = true
 	vim.opt.listchars:append "eol:↴"
 	
 	require("indent_blankline").setup {
     	show_end_of_line = true,
 	}
+
+	require('gitsigns').setup({
+	signs = {
+		add = {text = '▎'},
+		change = {text = '▎'},
+		delete = {text = '➤'},
+		topdelete = {text = '➤'},
+		changedelete = {text = '▎'},
+	}
+})
 end)
