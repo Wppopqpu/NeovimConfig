e64d91a6a71421ed5ef33dd7309d3c1eeefceef0
Wppopqpu <wppopqpu@outlook.com>
2023-05-06 18:21:01 +0800

    add lualine.nvim

--- Core/Plugins.lua
+++ Core/Plugins.lua
@@ -1,10 +1,19 @@
 -- Plugins.lua
 return require( 'packer' ).startup(function( use )
 	use 'wbthomason/packer.nvim'
+
 	use 'folke/tokyonight.nvim'
+
 	use 'lukas-reineke/indent-blankline.nvim'
+
 	use 'lewis6991/gitsigns.nvim'
-	use {'neovim/nvim-lspconfig', 'williamboman/nvim-lsp-installer'}
+
+	use 'neovim/nvim-lspconfig'
+	use 'williamboman/nvim-lsp-installer'
+
+	use 'kyazdani42/nvim-web-devicons'
+
+	use 'nvim-lualine/lualine.nvim'
 
 
 
@@ -12,17 +21,30 @@ return require( 'packer' ).startup(function( use )
 	vim.opt.list = true
 	vim.opt.listchars:append "eol:↴"
 	
-	require("indent_blankline").setup {
+	require("indent_blankline").setup({
     	show_end_of_line = true,
-	}
+	})
 
 	require('gitsigns').setup({
-	signs = {
-		add = {text = '▎'},
-		change = {text = '▎'},
-		delete = {text = '➤'},
-		topdelete = {text = '➤'},
-		changedelete = {text = '▎'},
-	}
-})
+		signs = {
+			add = {text = '▎'},
+			change = {text = '▎'},
+			delete = {text = '➤'},
+			topdelete = {text = '➤'},
+			changedelete = {text = '▎'},
+		},
+	})
+
+
+	require('lualine').setup({
+		options = {
+			theme = 'tokyonight',
+			icons_enabled = true,
+			component_separators = '|',
+			section_separators = ' ',
+		},
+	})
+
+
+
 end)
