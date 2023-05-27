e8ca3b14f892aae954219700224eb86af50fa97e
Wppopqpu <wppopqpu@outlook.com>
2023-05-27 18:13:28 +0800

    complete the treesitter configuration

--- Core/Plugins.lua
+++ Core/Plugins.lua
@@ -65,15 +65,19 @@ return require( 'packer' ).startup(function( use )
 		, 'c'
 		, 'python'},
 
+	highlight = {
+		enabled = true,
+		additional_vim_regex_highlighting = false,
 		disable = function(lang, buf)
-        	local max_filesize = 100 * 1024 -- 100 KB
-        	local ok, stats = pcall(
+			local max_filesize = 100 * 1024 -- 100 KB
+			local ok, stats = pcall(
 				vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf)
 			)
-        	if ok and stats and stats.size > max_filesize then
-            	return true
-        	end
-    	end,
-	}
+			if ok and stats and stats.size > max_filesize then
+				return true
+			end
+		end,
+		}
+	},
 
 end)
