fa77661a3e52749eb2a4f99d7cd98d9768d5f870
wppopqpu <wppopqpu@outlook.com>
2024-01-27 18:30:31 +0800

    Some changes to downloading lazy

--- Core/plugins.lua
+++ Core/plugins.lua
@@ -3,15 +3,20 @@ local path2Lazy = vim.fn.stdpath'data'..'lazy/lazy.nvim'
 
 -- if there is no lazy.nvim, we download it.
 if not vim.loop.fs_stat(path2Lazy) then
-	vim.cmd'echo "downloading lazy.nvim."'
-	vim.fn.system {
+	print'Downloading lazy.nvim.'
+	vim.system({
 		'git',
 		'clone',
 		'--filter=blob:none',
 		'https://github.com/folke/lazy.nvim.git',
 		'--branch=stable',
 		path2Lazy,
-	}
+	}, function(obj)
+		print(obj.code)
+		print(obj.signal)
+		print(obj.stdout)
+		print(obj.stderr)
+	end)
 end
 
 vim.opt.rtp:prepend(path2Lazy)
