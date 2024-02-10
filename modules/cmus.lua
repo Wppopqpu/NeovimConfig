c63664061e0a9d2bc1117b5e2d57b9bbbee9b10d
wppopqpu <wppopqpu@outlook.com>
2024-02-10 17:16:46 +0800

    add vim-cmus key bindings

--- modules/cmus.lua
+++ modules/cmus.lua
@@ -5,6 +5,19 @@ return {
 		config = function()
 			vim.cmd('source '..require'NeovimConfig.Core.lazypath'
 				.. 'vim-cmus/plugin/cmus.vim')
+			require'which-key'.register({
+				c = {
+					name = 'Cmus',
+					c = { ':Cmus<CR>', 'Menu' },
+					n = { ':CmusCurrent<CR>', 'Current' },
+					h = { ':CmusPrevious<CR>', 'Previous' },
+					l = { ':CmusNext<CR>', 'Next' },
+					z = { ':CmusPlay<CR>', 'Play' },
+					x = { ':CmusPause<CR>', 'Pause' },
+					d = { ':CmusStop<CR>', 'Stop' },
+				}
+			}, { prefix = '<leader>' })
+
 		end,
 	},
 }
