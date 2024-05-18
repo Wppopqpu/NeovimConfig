11ac06e64625c5d6064c6521594640b94cbee5d7
Wppopqpu <wppopqpu@outlook.com>
2024-05-18 13:13:38 +0800

    fix missing vim.iter by copy file

--- Entry.lua
+++ Entry.lua
@@ -1,4 +1,10 @@
 -- Entry.lua
+-- fix missing vim.iter
+if nil == vim.iter then
+	vim.iter = require"NeovimConfig.details.iter"
+else
+	print"[NeovimConfig/Entry.lua]:vim.iter is fixed"
+end
 
 require'NeovimConfig.Core.Options'
 require'NeovimConfig.Core.plugins'
