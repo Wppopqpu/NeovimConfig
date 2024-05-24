e825745c9c9515323495cbf7887314be1e3e30bb
Wppopqpu <wppopqpu@outlook.com>
2024-05-24 19:32:29 +0800

    chore(.):remove startup message

--- Entry.lua
+++ Entry.lua
@@ -2,8 +2,6 @@
 -- fix missing vim.iter
 if nil == vim.iter then
 	vim.iter = require"NeovimConfig.details.iter"
-else
-	print"[NeovimConfig/Entry.lua]:vim.iter is fixed"
 end
 
 require'NeovimConfig.Core.Options'
