beffa95d31d29d283608078788294c5dcc35a9b6
Wppopqpu <wppopqpu@outlook.com>
2024-05-31 21:44:56 +0800

    fix(lua):add table.merge

--- Entry.lua
+++ Entry.lua
@@ -4,5 +4,9 @@ if nil == vim.iter then
 	vim.iter = require"NeovimConfig.details.iter"
 end
 
+if nil == table.merge then
+	table.merge = require("NeovimConfig.details.table_merge")
+end
+
 require'NeovimConfig.Core.Options'
 require'NeovimConfig.Core.plugins'
