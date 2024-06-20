b3eed9055873aa6e4bb13a3f123047dda13817d8
wppopqpu <wppopqpu@outlook.com>
2024-06-21 06:46:05 +0800

    feat:add cppman support

--- modules/utils.lua
+++ modules/utils.lua
@@ -27,4 +27,26 @@ return {
 			}
 		end,
 	},
+	{
+		"madskjeldgaard/cppman.nvim",
+		event = "User LazyFt cpp",
+		dependencies = {
+			"MunifTanjim/nui.nvim",
+		},
+		config = function()
+			-- what can i say
+			local man = require("cppman")
+			man.setup()
+
+			require("which-key").register({
+				r = {
+					name = "cppman",
+					m = { function ()
+						man.open_manual_for(vim.fn.expand("<cword>"))
+					end, "for current word" },
+					c = { man.input, "search" },
+				},
+			}, { prefix = "<leader>" })
+		end,
+	},
 }
