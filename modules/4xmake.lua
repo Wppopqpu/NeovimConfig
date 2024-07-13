5cadf874ae4b5b9a8fc2e4647f56c0c80e049fed
Wppopqpu <wppopqpu@outlook.com>
2024-07-13 20:26:35 +0800

    feat: update xmake config

--- modules/4xmake.lua
+++ modules/4xmake.lua
@@ -18,7 +18,7 @@ return {
 			require'xmake'.setup{}
 			local xmakeComponent = {
 				function()
-					local xmake = require'xmake.project_config'.info
+					local xmake = require'xmake.project'.info
 					if xmake.target.tg == '' then
 						return ''
 					end
@@ -28,7 +28,7 @@ return {
 					return vim.o.columns > 100
 				end,
 				on_click = function()
-					require'xmake.project_config.menu'.init()
+					require'xmake.project._menu'.init()
 				end,
 			}
 			require'lualine'.setup {
