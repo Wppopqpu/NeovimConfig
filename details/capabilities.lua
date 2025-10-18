520d1a629f9f9d82427334672f508081ef201a80
Wppopqpu <wppopqpu@outlook.com>
2025-10-19 05:51:56 +0800

    fix(typo): in capabilities.lua

--- details/capabilities.lua
+++ details/capabilities.lua
@@ -24,7 +24,7 @@ local function test_capability(c, name)
 end
 
 function M.get_filter(c, to_remove, delete_action)
-	to_remove = to_remove or true
+	local to_remove = to_remove or true
 	local delete_action = delete_action or function (t, i)
 		t[i].enabled = false
 	end
