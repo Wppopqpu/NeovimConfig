5bb721dcc744fe0309aa26e00065de5fdd0871f7
Wppopqpu <wppopqpu@outlook.com>
2026-05-17 19:29:50 +0800

    feat: add tsqx support

--- core/options.lua
+++ core/options.lua
@@ -14,6 +14,7 @@ vim.filetype.add{
 		ixx = 'cpp',
 		cppm = 'cpp',
 		mxx = 'cpp',
+		tsqx = "tsqx",
 	},
 }
 
--- modules/treesitter.lua
+++ modules/treesitter.lua
@@ -22,6 +22,20 @@ return g_filter {
 		build = function()
 			vim.cmd'TSUpdate'
 		end,
+		init = function ()
+			local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+			parser_config.tsqx = {
+				install_info = {
+					url = "https://github.com/extouchtriangle/tree-sitter-tsqx", -- local path or git repo
+					files = { "src/parser.c" }, -- note that some parsers also require src/scanner.c or src/scanner.cc
+					-- optional entries:
+					branch = "main", -- default branch in case of git repo if different from master
+					generate_requires_npm = false, -- if stand-alone parser without npm dependencies
+					requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
+				},
+				filetype = "tsqx", -- if filetype does not match the parser name
+			}
+		end,
 		config = function()
 			require'nvim-treesitter.install'.prefer_git = true
 			require'nvim-treesitter.configs'.setup{
